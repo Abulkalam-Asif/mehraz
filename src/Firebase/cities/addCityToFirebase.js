c67b6dce8a78b83b3fce632c1a97876200e6743f
Sniper512 <alimuhammadjafar873@gmail.com>
2024-02-21 17:49:59 +0500

    City Security Rules

--- src/Firebase/cities/addCityToFirebase.js
+++ src/Firebase/cities/addCityToFirebase.js
@@ -2,24 +2,33 @@
 import { revalidatePath } from "next/cache";
 import { db } from "../firebase";
 import { addDoc, collection } from "firebase/firestore";
+import { query, where, getDocs } from "firebase/firestore";
 
 const addCityToDB = async ({ name, usage }) => {
-  const ref = collection(db, "CITIES");
-  // TODO: Add a check to see if the city already exists
-  try {
-    await addDoc(ref, {
-      name,
-      usage,
-    });
-    revalidatePath("/admin/roles-analytics-cities", "page");
-    return { type: "success", message: "City added successfully!" };
-  } catch (err) {
-    console.error("Error adding the city: " + err);
-    return {
-      type: "error",
-      message: "Something went wrong, please try again later.",
-    };
-  }
+	const ref = collection(db, "CITIES");
+
+	const queryResult = query(ref, where("name", "==", name));
+
+	const querySnapshot = await getDocs(queryResult);
+
+	if (!querySnapshot.empty) {
+		return { type: "error", message: "City with this name already exists." };
+	}
+
+	try {
+		await addDoc(ref, {
+			name,
+			usage,
+		});
+		revalidatePath("/admin/roles-analytics-cities", "page");
+		return { type: "success", message: "City added successfully!" };
+	} catch (err) {
+		console.error("Error adding the city: " + err);
+		return {
+			type: "error",
+			message: "Something went wrong, please try again later.",
+		};
+	}
 };
 
 export default addCityToDB;
--- src/Firebase/cities/updateCityFromFirebase.js
+++ src/Firebase/cities/updateCityFromFirebase.js
@@ -1,33 +1,51 @@
 "use server";
 import { revalidatePath } from "next/cache";
 import { db } from "../firebase";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import {
+	doc,
+	getDoc,
+	updateDoc,
+	collection,
+	query,
+	where,
+	getDocs,
+} from "firebase/firestore";
 
 const updateCityInDB = async ({ id, name }) => {
-  try {
-    const cityRef = doc(db, "CITIES", id);
-    const docSnapshot = await getDoc(cityRef);
+	try {
+		const cityRef = doc(db, "CITIES", id);
 
-    if (docSnapshot.exists()) {
-      await updateDoc(cityRef, {
-        name: name,
-        usage: docSnapshot.data().usage,
-      });
-      revalidatePath("/admin/roles-analytics-cities", "page");
-      return { type: "success", message: "City updated successfully!" };
-    } else {
-      return {
-        type: "error",
-        message: "Something went wrong, please try again later.",
-      };
-    }
-  } catch (error) {
-    console.error("Error updating the city:", error);
-    return {
-      type: "error",
-      message: "Something went wrong, please try again later.",
-    };
-  }
+		const citiesCollectionRef = collection(db, "CITIES");
+		const queryResult = query(citiesCollectionRef, where("name", "==", name));
+
+		const querySnapshot = await getDocs(queryResult);
+
+		if (!querySnapshot.empty) {
+			return { type: "error", message: "City with this name already exists." };
+		}
+
+		const docSnapshot = await getDoc(cityRef);
+
+		if (docSnapshot.exists()) {
+			await updateDoc(cityRef, {
+				name: name,
+				usage: docSnapshot.data().usage,
+			});
+			revalidatePath("/admin/roles-analytics-cities", "page");
+			return { type: "success", message: "City updated successfully!" };
+		} else {
+			return {
+				type: "error",
+				message: "Something went wrong, please try again later.",
+			};
+		}
+	} catch (error) {
+		console.error("Error updating the city:", error);
+		return {
+			type: "error",
+			message: "Something went wrong, please try again later.",
+		};
+	}
 };
 
 export default updateCityInDB;
