6994b9b47e6ea4c1643ca45d42c3611e1f1ba8a2
Sniper512 <alimuhammadjafar873@gmail.com>
2024-02-27 00:12:36 +0500

    add Currency Rules

--- src/Firebase/currencies/addCurrencyToFirebase.js
+++ src/Firebase/currencies/addCurrencyToFirebase.js
@@ -2,42 +2,67 @@
 import { revalidatePath } from "next/cache";
 import { db } from "../firebase";
 import {
-  addDoc,
-  collection,
-  doc,
-  updateDoc,
-  increment,
+	addDoc,
+	collection,
+	doc,
+	updateDoc,
+	increment,
+	where,
+	query,
+	getDocs,
+	getDoc,
 } from "firebase/firestore";
 
 const addCurrencyToDB = async ({ name, valueInPkr, cities, usage }) => {
-  const ref = collection(db, "CURRENCIES");
+	const ref = collection(db, "CURRENCIES");
 
-  try {
-    await Promise.all(
-      cities.map(async (id) => {
-        const cityRef = doc(db, "CITIES", id);
-        await updateDoc(cityRef, {
-          [`usage.currencies`]: increment(1),
-        });
-      })
-    );
+	const queryResult = query(ref, where("name", "==", name));
+	const querySnapshot = await getDocs(queryResult);
 
-    await addDoc(ref, {
-      name,
-      valueInPkr,
-      cities,
-      usage,
-    });
+	if (!querySnapshot.empty) {
+		return {
+			type: "error",
+			message: "Currency with this name already exists.",
+		};
+	}
 
-    revalidatePath("/admin/roles-analytics-cities", "page");
-    return { type: "success", message: "Currency added successfully!" };
-  } catch (err) {
-    console.error("Error adding the currency:", err);
-    return {
-      type: "error",
-      message: "Something went wrong, please try again later.",
-    };
-  }
+	
+	try {
+		const cityExistPromises = cities.map(async (id) => {
+			const cityDoc = doc(db, "CITIES", id);
+			const citySnapshot = await getDoc(cityDoc);
+			if (!citySnapshot.exists()) {
+				throw new Error(`City with ID ${id} does not exist.`);
+			}
+		});
+
+		await Promise.all(cityExistPromises);
+
+		await Promise.all(
+			cities.map(async (id) => {
+				const cityRef = doc(db, "CITIES", id);
+				await updateDoc(cityRef, {
+					[`usage.currencies`]: increment(1),
+				});
+			})
+		);
+
+		await addDoc(ref, {
+			name,
+			valueInPkr,
+			cities,
+			usage,
+		});
+
+		revalidatePath("/admin/roles-analytics-cities", "page");
+		return { type: "success", message: "Currency added successfully!" };
+	} catch (err) {
+		console.error("Error adding the currency:", err);
+		return {
+			type: "error",
+			message: "Something went wrong, please try again later.",
+		};
+	}
 };
 
 export default addCurrencyToDB;
