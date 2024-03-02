810cdcf5cf0b21a2628c56bf43726c42a8bdb872
Sniper512 <alimuhammadjafar873@gmail.com>
2024-03-02 15:21:21 +0500

    Currency update and Delete rules

--- src/Firebase/currencies/updateCurrencyFromFirebase.js
+++ src/Firebase/currencies/updateCurrencyFromFirebase.js
@@ -1,57 +1,79 @@
 "use server";
 import { revalidatePath } from "next/cache";
 import { db } from "../firebase";
-import { doc, getDoc, updateDoc, increment } from "firebase/firestore";
+import {
+	doc,
+  collection,
+	getDoc,
+	updateDoc,
+	increment,
+	where,
+	query,
+	getDocs,
+} from "firebase/firestore";
 
 const updateCurrencyInDB = async (currentCurrency, prevCities) => {
-  let { id, name, cities, valueInPkr } = currentCurrency;
-  try {
-    const currencyRef = doc(db, "CURRENCIES", id);
-    const docSnapshot = await getDoc(currencyRef);
+	let { id, name, cities, valueInPkr } = currentCurrency;
+	const ref = collection(db, "CURRENCIES");
 
-    const newlyAddedCities = cities.filter(
-      (cityId) => !prevCities.includes(cityId)
-    );
+	
+	try {
+		const currencyExistPromises = cities.map(async (id) => {
+			const cityDoc = doc(db, "CITIES", id);
+			const citySnapshot = await getDoc(cityDoc);
+			if (!citySnapshot.exists()) {
+				throw new Error(`City with ID ${id} does not exist.`);
+			}
+		});
 
-    newlyAddedCities.forEach(async (cityId) => {
-      const cityRef = doc(db, "CITIES", cityId);
-      await updateDoc(cityRef, {
-        [`usage.currencies`]: increment(1),
-      });
-    });
+		await Promise.all(currencyExistPromises);
 
-    prevCities.forEach(async (cityId) => {
-      if (!cities.includes(cityId)) {
-        const cityRef = doc(db, "CITIES", cityId);
-        await updateDoc(cityRef, {
-          [`usage.currencies`]: increment(-1),
-        });
-      }
-    });
+		const newlyAddedCities = cities.filter(
+			(cityId) => !prevCities.includes(cityId)
+		);
 
-    if (docSnapshot.exists()) {
-      await updateDoc(currencyRef, {
-        name: name,
-        valueInPkr: valueInPkr,
-        cities: cities,
-        usage: docSnapshot.data().usage,
-      });
+		const currencyRef = doc(db, "CURRENCIES", id);
+		const docSnapshot = await getDoc(currencyRef);
 
-      revalidatePath("/admin/roles-analytics-cities", "page");
-      return { type: "success", message: "Currency updated successfully!" };
-    } else {
-      return {
-        type: "error",
-        message: "Something went wrong, please try again later.",
-      };
-    }
-  } catch (error) {
-    console.error("Error updating the currency:", error);
-    return {
-      type: "error",
-      message: "Something went wrong, please try again later.",
-    };
-  }
+		if (docSnapshot.exists()) {
+			await updateDoc(currencyRef, {
+				name: name,
+				valueInPkr: valueInPkr,
+				cities: cities,
+				usage: docSnapshot.data().usage,
+			});
+
+			newlyAddedCities.forEach(async (cityId) => {
+				const cityRef = doc(db, "CITIES", cityId);
+				await updateDoc(cityRef, {
+					[`usage.currencies`]: increment(1),
+				});
+			});
+
+			prevCities.forEach(async (cityId) => {
+				if (!cities.includes(cityId)) {
+					const cityRef = doc(db, "CITIES", cityId);
+					await updateDoc(cityRef, {
+						[`usage.currencies`]: increment(-1),
+					});
+				}
+			});
+
+			revalidatePath("/admin/roles-analytics-cities", "page");
+			return { type: "success", message: "Currency updated successfully!" };
+		} else {
+			return {
+				type: "error",
+				message: "Something went wrong, please try again later.",
+			};
+		}
+	} catch (error) {
+		console.error("Error updating the currency:", error);
+		return {
+			type: "error",
+			message: "Something went wrong, please try again later.",
+		};
+	}
 };
 
 export default updateCurrencyInDB;
