d186d2f69ab1c52ccebc6197ee34838143cbe614
Sniper512 <alimuhammadjafar873@gmail.com>
2024-04-23 18:37:41 +0500

    Update Second Screen

--- src/Firebase/admin-side/ready_project/updateReadyProjectScreen-2.js
+++ src/Firebase/admin-side/ready_project/updateReadyProjectScreen-2.js
@@ -6,17 +6,18 @@ import {
   deleteDoc,
   doc,
   getDoc,
+  getDocs,
   updateDoc,
+  arrayUnion,
 } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 
 const updateReadyProjectS2ToDB = async ({ id, designs, budgetRanges }) => {
   try {
     const readyProjectsRef = collection(db, "READY_PROJECTS");
-
     const readyProjectDocRef = doc(readyProjectsRef, id);
-
     const readyProjectDoc = await getDoc(readyProjectDocRef);
+
     if (!readyProjectDoc.exists()) {
       console.error("No document found!");
       return {
@@ -26,58 +27,76 @@ const updateReadyProjectS2ToDB = async ({ id, designs, budgetRanges }) => {
       };
     }
 
-    // Getting previous designs from the database
     const readyProjectPrevDesigns = readyProjectDoc.data().designs;
     const rpDesignsIds = [];
-    if (readyProjectPrevDesigns?.length > 0) {
-      await Promise.all(
-        readyProjectPrevDesigns?.map(async prevDesignId => {
-          const prevDesignDocRef = doc(
-            collection(db, "RP_DESIGNS"),
-            prevDesignId,
-          );
-          const prevDesignData = (await getDoc(prevDesignDocRef)).data();
-          if (
-            designs.find(
-              newDesign =>
-                newDesign.areaId === prevDesignData.areaId &&
-                newDesign.floorId === prevDesignData.floorId &&
-                newDesign.familyUnitId === prevDesignData.familyUnitId,
-            )
-          ) {
-            // If any of the previous designs is present in the new designs, then add it to alreadyAddedDesigns array and remove it from the new designs array
-            rpDesignsIds.push(prevDesignId);
-            designs = designs.filter(
-              newDesign =>
-                newDesign.areaId !== prevDesignData.areaId ||
-                newDesign.floorId !== prevDesignData.floorId ||
-                newDesign.familyUnitId !== prevDesignData.familyUnitId,
-            );
-          } else {
-            // If any of the previous designs is not present in the new designs, then delete it from the database
-            await deleteDoc(prevDesignDocRef);
-            const storageRef = ref(storage, `RP_DESIGNS/${prevDesignId}`);
-            deleteObject(storageRef)
-              .then(() => {})
-              .catch(error => {});
-          }
-        }),
-      );
-    }
+    const rpDesignsData = [];
+    let newDesigns = [];
 
-    // Add the remaining new designs to the database
-    const rpDesignsRef = collection(db, "RP_DESIGNS");
+    console.log("Ready project designs: ", readyProjectPrevDesigns);
+
+    // Delete designs from RP_DESIGNS collection and ready project which are not included in the selected designs
 
     await Promise.all(
-      designs.map(async design => {
-        const response = await addDoc(rpDesignsRef, {
-          ...design,
-        });
-        rpDesignsIds.push(response.id);
+      readyProjectPrevDesigns?.map(async prevDesignId => {
+        const prevDesignDocRef = doc(
+          collection(db, "RP_DESIGNS"),
+          prevDesignId,
+        );
+
+        const prevDesignData = (await getDoc(prevDesignDocRef)).data();
+
+        rpDesignsData.push(prevDesignData);
+        const matchingNewDesign = designs.find(
+          newDesign =>
+            newDesign.areaId === prevDesignData.areaId &&
+            newDesign.floorId === prevDesignData.floorId &&
+            newDesign.familyUnitId === prevDesignData.familyUnitId,
+        );
+
+        if (!matchingNewDesign) {
+          console.log("Not matching Design: ", prevDesignId);
+          // Remove design from RP_DESIGNS collection
+          await deleteDoc(prevDesignDocRef);
+          const storageRef = ref(storage, `RP_DESIGNS/${prevDesignId}`);
+          await deleteObject(storageRef).catch(error => {
+            console.error("Error deleting object from storage: ", error);
+          });
+        } else {
+          rpDesignsIds.push(prevDesignId);
+        }
       }),
     );
 
-    // Update the ready project document
+    // console.log("Designs after deletion: ", designs);
+    console.log("Prev designs array: ", rpDesignsData);
+    console.log("Prev designs IDS: ", rpDesignsIds);
+
+
+    // Obtain a third array containing objects from array1 that are not present in array2
+    newDesigns = designs.filter(
+      obj1 =>
+        !rpDesignsData.some(
+          obj2 =>
+            obj1.areaId === obj2.areaId &&
+            obj1.familyUnitId === obj2.familyUnitId &&
+            obj1.floorId === obj2.floorId,
+        ),
+    );
+
+    console.log("New Data: ", newDesigns);
+
+    // Add the remaining new designs to the database
+    const rpDesignsRef = collection(db, "RP_DESIGNS");
+ 
+    await Promise.all(
+      newDesigns.map(async design => {
+        const response = await addDoc(rpDesignsRef, design);
+        rpDesignsIds.push(response.id);
+      }),
+    );
+    console.log("Prev designs IDS Below: ", rpDesignsIds);
+
+    //Update the ready project document
     await updateDoc(readyProjectDocRef, {
       designs: rpDesignsIds,
       budgetRanges,
@@ -86,10 +105,10 @@ const updateReadyProjectS2ToDB = async ({ id, designs, budgetRanges }) => {
     return {
       data: rpDesignsIds,
       type: "SUCCESS",
-      message: "Ready project screen 2 added successfully!",
+      message: "Ready project screen 2 updated successfully!",
     };
   } catch (error) {
-    console.error("Error adding the ready project: ", error.message);
+    console.error("Error updating the ready project: ", error.message);
     return {
       data: null,
       type: "ERROR",
